d0423330877ca9766ba28b2db2349eaa72abee74
Z-Jian <2002689@sit.singaporetech.edu.sg>
2022-10-03 09:28:29 +0800

    added toyota project as static

---
+++ Toyota_Smarthome/pipline/run_PDAN.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+export PATH=/home/rdai/anaconda3/envs/torch1.9/bin:$PATH
+
+# python train.py \
+# -gpu 0 \
+# -dataset TSU \
+# -mode rgb \
+# -split_setting CS \
+# -model PDAN \
+# -load_model ./PDAN_TSU_RGB \
+# -train True \
+# -num_channel 512 \
+# -lr 0.0002 \
+# -kernelsize 3 \
+# -APtype map \
+# -epoch 140 \
+# -batch_size 2 \
+# -comp_info TSU_CS_RGB_PDAN
+
+python train.py \
+-dataset TSU \
+-mode rgb \
+-split_setting CS \
+-model PDAN \
+-train True \
+-num_channel 512 \
+-lr 0.0002 \
+-kernelsize 3 \
+-APtype map \
+-epoch 140 \
+-batch_size 2 \
+-comp_info TSU_CS_RGB_PDAN
